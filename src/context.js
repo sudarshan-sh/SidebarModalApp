b175726f841d99ac78b6d9c87fc3c6b0bbb59eaa
Sudarshan <mail2sudarshan.sharma@gmail.com>
2022-03-17 14:51:00 +0530

    first set of development

---
+++ src/context.js
@@ -0,0 +1,35 @@
+import React, { useContext, useState } from 'react';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    }
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    }
+
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    }
+    const closeModal = () => {
+        setIsModalOpen(false);
+    }
+
+    return <AppContext.Provider value={{ isSidebarOpen,isModalOpen,openSidebar,closeSidebar,
+        openModal,closeModal }}>
+        { children }
+    </AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+    // useContext returns the current context value
+    return useContext(AppContext);
+}
+
+export { AppProvider, AppContext };
